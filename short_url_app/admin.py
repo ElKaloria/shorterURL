ad336ece4bcbf5d91b16c2fcc8396458a387537e
ElKaloria <maks.dektyarev@yandex.ru>
2024-08-15 12:52:08 +0700

    register URL model in admin

--- short_url_app/admin.py
+++ short_url_app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import ShortUrl
+
 
 # Register your models here.
+
+@admin.register(ShortUrl)
+class URLAdmin(admin.ModelAdmin):
+    list_display = ('original_url', 'short_url', 'request_count', 'created_at', 'user')
+    search_fields = ('original_url', 'short_url')
+    ordering = ['-created_at']
+
