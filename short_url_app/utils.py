cf74a49ccfd5a1172ab50ece45512d7b6f7c5f72
ElKaloria <maks.dektyarev@yandex.ru>
2024-08-16 17:11:27 +0700

    add original url exist and get short orl functions

--- short_url_app/utils.py
+++ short_url_app/utils.py
@@ -11,3 +11,18 @@ def get_original_url(url: str) -> str:
     full_url.request_count += 1
     full_url.save()
     return full_url.original_url
+
+
+def original_url_exist(url: str) -> bool:
+    try:
+        ShortUrl.objects.get(original_url__exact=url)
+        return True
+    except ShortUrl.DoesNotExist:
+        return False
+
+
+def get_short_url(url: str) -> str:
+    try:
+        return ShortUrl.objects.get(original_url__exact=url).short_url
+    except ShortUrl.DoesNotExist:
+        raise KeyError("This url does not exist")
