4a166ef48008d7b374cdb279a904a7ec093b511c
ElKaloria <maks.dektyarev@yandex.ru>
2024-08-22 11:49:19 +0700

    add debug-toolbar urls

--- ShorterURL/urls.py
+++ ShorterURL/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import debug_toolbar
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
@@ -24,3 +26,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('short_url_app.urls')),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
